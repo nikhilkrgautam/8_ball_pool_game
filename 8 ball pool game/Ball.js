0a35806a9f9daa00899fef4b0dfd1ee5da7ebc3e
NIKHIL KUMAR GAUTAM <44256345+nikhilkrgautam@users.noreply.github.com>
2019-03-28 18:46:35 +0530

    Add files via upload

---
+++ 8 ball pool game/Ball.js
@@ -0,0 +1,128 @@
+const BALL_ORIGIN = new Vector2(25,25)
+const BALL_DIAMETER =  38;
+const BALL_RADIUS = BALL_DIAMETER/2;
+
+function Ball(position, color)
+{
+    this.position = position;
+    this.velocity = new Vector2();
+    this.moving = false;
+    this.sprite = getBallSpriteByColor(color);
+}
+
+Ball.prototype.update = function(delta)
+{
+this.position.addTo(this.velocity.mult(delta));
+this.velocity = this.velocity.mult(0.986); //Apply friction
+
+if(this.velocity.length() < 5)
+{
+    this.velocity = new Vector2();
+    this.moving = false;
+}
+}
+
+Ball.prototype.draw = function()
+{
+    Canvas.drawImage(this.sprite, this.position, BALL_ORIGIN);
+}
+
+Ball.prototype.shoot = function(power, rotation)
+{
+    this.velocity = new Vector2(power * Math.cos(rotation), power * Math.sin(rotation));
+    this.moving = true;
+}
+
+Ball.prototype.collideWithBall = function(ball)
+{
+//Find a normal vector
+const n = this.position.subtract(ball.position);
+
+//Find distance
+const dist = n.length();
+
+if(dist > BALL_DIAMETER)
+{
+    return;
+}
+//Find min. translation distance
+const mtd = n.mult((BALL_DIAMETER - dist) / dist);
+
+//Push-pull balls apart
+this.position = this.position.add(mtd.mult(1/2));
+ball.position = ball.position.subtract(mtd.mult(1/2));
+
+//Find unit normal vector
+const un = n.mult(1/n.length());
+
+//Find unit tangent vector
+const ut = new Vector2(-un.y, un.x);
+
+//Project velocities onto the unit normal and unit tangent vectors.
+const v1n = un.dot(this.velocity);
+const v1t = ut.dot(this.velocity);
+const v2n = un.dot(ball.velocity);
+const v2t = ut.dot(ball.velocity);
+
+//Find new normal velocities
+let v1nTag = v2n;
+let v2nTag = v1n;
+
+//Convert the scalar normal and tangential velocities into vectors
+v1nTag = un.mult(v1nTag);
+const v1tTag = ut.mult(v1t);
+v2nTag = un.mult(v2nTag);
+const v2tTag = ut.mult(v2t);
+
+//Update velocities
+this.velocity = v1nTag.add(v1tTag);
+ball.velocity = v2nTag.add(v2tTag);
+
+this.moving = true;
+ball.moving = true; 
+}
+
+Ball.prototype.collideWithTable = function(table)
+{
+  if(!this.moving)
+  {
+      return;
+  }
+  let collided = false;
+
+  if(this.position.y <= table.TopY + BALL_RADIUS)
+  {
+      this.position.y = table.TopY + BALL_RADIUS;
+      this.velocity = new Vector2(this.velocity.x, -this.velocity.y);
+      collided = true;
+  }
+
+  if(this.position.x >= table.RightX - BALL_RADIUS)
+  {
+      this.position.x = table.RightX - BALL_RADIUS;
+      this.velocity = new Vector2(-this.velocity.x, this.velocity.y);
+      collided = true;
+  }
+
+  if(this.position.y >= table.BottomY - BALL_RADIUS)
+  {
+      this.position.y = table.BottomY - BALL_RADIUS;
+      this.velocity = new Vector2(this.velocity.x, -this.velocity.y);
+      collided = true;
+  } 
+
+  if(this.position.x <= table.LeftX + BALL_RADIUS)
+  {
+      this.position.x = table.LeftX + BALL_RADIUS;
+      this.velocity = new Vector2(-this.velocity.x, this.velocity.y);
+      collided = true;
+  }
+
+  if(collided)
+  {
+      this.velocity = this.velocity.mult(0.98);
+  }
+}
+
+
+
---
+++ 8 ball pool game/Stick.js
@@ -0,0 +1,71 @@
+const STICK_ORIGIN = new Vector2(970, 11);
+const STICK_SHOT_ORIGIN = new Vector2(950, 11);
+const MAX_POWER = 8000;
+
+function Stick(position, onShoot) 
+{
+    this.position = position;
+    this.rotation = 0;
+    this.origin = STICK_ORIGIN.copy();
+    this.power = 0;
+    this.onShoot = onShoot;
+    this.shot = false; 
+}
+
+Stick.prototype.update = function()
+{
+    if(this.shot)
+    {
+        return;
+    }
+
+    if(Mouse.left.down)
+    {
+        this.increasePower();
+    }
+    else if(this.power >0)
+    {
+        this.shoot();
+    }
+
+    this.updateRotation();
+}
+
+Stick.prototype.draw = function()
+{
+    Canvas.drawImage(sprites.stick, this.position, this.origin, this.rotation);
+}
+
+Stick.prototype.updateRotation = function()
+{
+    let opposite = Mouse.position.y - this.position.y;
+    let adjacent = Mouse.position.x - this.position.x;
+
+    this.rotation = Math.atan2(opposite, adjacent);
+}
+
+Stick.prototype.increasePower = function()
+{
+    if(this.power > MAX_POWER)
+    {
+        return;
+    }
+ 
+    this.power += 122;
+    this.origin.x +=5;
+}
+
+Stick.prototype.shoot = function()
+{
+    this.onShoot(this.power, this.rotation);
+    this.power = 0;
+    this.origin = STICK_SHOT_ORIGIN.copy();
+    this.shot = true;
+}
+
+Stick.prototype.reposition = function(position)
+{
+    this.position = position.copy();
+    this.origin = STICK_ORIGIN.copy();
+    this.shot = false;
+}
